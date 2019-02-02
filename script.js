16e51b5330716c2303f5172254e86496bade97ad
Kevin Li <kevinli020508@gmail.com>
2019-02-02 13:22:48 -0500

    Truly update calculatorType and related variable settings using radio buttons

--- script.js
+++ script.js
@@ -2,29 +2,34 @@
 let calculatorType = "color";
 document.querySelector(`input#${calculatorType}`).setAttribute("checked", true);
 document.querySelectorAll('input[name="calculatorType"]')
-.forEach((el) => {
-    el.addEventListener("change", (e) => {
-    calculatorType = e.target.value;
-	// console.log('TCL: e.target', e.target);
-})});
-let lineLength, rowLength;
-// set up variables for different calculators
-switch (calculatorType){
-    case "monochrome": // e.g. TI-83
-        lineLength = 16;
-        rowLength = 8;
-    break;
-    case "color": // e.g. TI-84/CSE/CE
-        lineLength = 26;
-        rowLength = 10;
-    break;
+    .forEach((el) => {
+        el.addEventListener("change", (e) => {
+            calculatorType = e.target.value;
+            changeCalculatorType();
+            // console.log('TCL: e.target', e.target);
+        })
+    });
+let lineLength, rowLength, menuTitleLength, menuItemLength;
+changeCalculatorType();
+function changeCalculatorType() {
+    // set up variables for different calculators
+    switch (calculatorType) {
+        case "monochrome": // e.g. TI-83
+            lineLength = 16;
+            rowLength = 8;
+            break;
+        case "color": // e.g. TI-84/CSE/CE
+            lineLength = 26;
+            rowLength = 10;
+            break;
+    }
+    menuTitleLength = lineLength;
+    menuItemLength = lineLength - 2;
 }
-const menuTitleLength = lineLength;
-const menuItemLength = lineLength - 2;
 
 const newFolderBtn = document.getElementById("newFolderBtn");
 const system = document.getElementById("system");
-newFolderBtn.addEventListener("click",(e) => {
+newFolderBtn.addEventListener("click", (e) => {
     let newFolder = document.createElement("input");
     newFolder.setAttribute("type", "text");
     newFolder.placeholder = "Enter folder name here";
