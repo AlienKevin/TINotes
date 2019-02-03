49bb8634e55e0bbf9dd79d56112da124b359fea9
Kevin Li <kevinli020508@gmail.com>
2019-02-03 14:15:35 -0500

    Be able to download the generated script with linebreaks in windows format (\r\n)

--- generateScript.js
+++ generateScript.js
@@ -1,8 +1,13 @@
 const generateScriptBtn = document.getElementById("generateScriptBtn");
 generateScriptBtn.addEventListener("click", exportScript);
+const downloadScriptBtn = document.getElementById("downloadScriptBtn");
+let script;
+downloadScriptBtn.addEventListener("click", () => {
+    download("TINOTES.txt", script);
+});
 
 function exportScript() {
-    const script = generateScript();
+    generateScript();
     const popupBody = document.querySelector('#popup div.modal-body');
     let viewer = document.getElementById("viewer")
     if (viewer) {
@@ -28,7 +33,22 @@ function generateScript() {
         branching += `Goto 0\n`;
     });
     homeMenu += `)`;
-    const script = `${homeMenu}\n${branching}Lbl 0\n${baseScript}`;
+    script = `${homeMenu}\n${branching}Lbl 0\n${baseScript}`;
+    script = script.replace(/\n/g, "\r\n");
     // console.log('TCL: generateScript -> script', script);
-    return script;
+}
+
+// Source: https://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
+// Start downloading a file in browser
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
 }
