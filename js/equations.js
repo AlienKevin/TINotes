e181c0f3fec5dba4466fcb4e37c6708122630725
Kevin Li <kevinli020508@gmail.com>
2019-02-21 07:42:26 -0500

    Can enter variable name with subscripts in guppy editor

--- js/equations.js
+++ js/equations.js
@@ -79,7 +79,7 @@ function convertTextToSymbol(textEquation) {
     Object.entries(equationTextToSymbol).forEach(([text, symbol]) => {
         textEquation = textEquation.replace(new RegExp(RegExp.escape(text), "g"), symbol);
     });
-    console.log('TCL: convertTextToSymbol -> textEquation', textEquation);
+    // console.log('TCL: convertTextToSymbol -> textEquation', textEquation);
     return textEquation;
 }
 
@@ -92,20 +92,81 @@ function convertSymbolToText(symbolEquation) {
 
 function getEquation() {
     try {
-        const equation = convertTextToSymbol(guppyInput.engine.get_content("text"));
+        const equation = handleSubscripts(convertTextToSymbol(guppyInput.engine.get_content("text")), true);
         detachWarning(eqInput);
         return equation;
     } catch (e) {
         warningTimerId = setTimeout(() => {
             AttachWarning(eqInput)
         }, 500);
-        console.log('TCL: timerId -> timerId', warningTimerId);
+        // console.log('TCL: timerId -> timerId', warningTimerId);
     }
-    return ""; // no equation found
+    return new Error("error loading equation");
+}
+
+function handleVarNameSubscripts(varName){
+    if (varName.indexOf("_") >= 0){
+        return handleSubscripts(`(${varName})`, true);
+    } else{
+        return varName;
+    }
+}
+
+function handleSubscripts(equation, withParentheses = true) {
+    if (equation.indexOf("_") >= 0) { // contains subscripts
+        let startIndex = 0;
+        let underscoreIndex = equation.indexOf("_", startIndex);
+        while (underscoreIndex >= 0) {
+            // console.log("underscoreIndex: ", underscoreIndex);
+            const openParenIndex = equation.lastIndexOf("(", underscoreIndex);
+            // console.log("openParenIndex: ", openParenIndex);
+            let closeParenIndex = findClosingBracketMatchIndex(equation, openParenIndex);
+            // console.log("closeParenIndex: ", closeParenIndex);
+            let subscriptStartIndex, subscriptEndIndex;
+            if (withParentheses) {
+                if (underscoreIndex + 1 < equation.length && equation.charAt(underscoreIndex + 1) !== "(") { // no open paren after underscore
+                    // surround the single subscript with a pair of parentheses
+                    equation = insertSubstring(equation, underscoreIndex + 1, "(");
+                    equation = insertSubstring(equation, closeParenIndex + 1, ")");
+                    closeParenIndex += 2; // two parentheses added
+                    // console.log("equation: " + equation);
+                }
+                subscriptStartIndex = underscoreIndex + 2;
+                subscriptEndIndex = closeParenIndex - 1;
+            } else {
+                if (underscoreIndex + 1 < equation.length && equation.charAt(underscoreIndex + 1) === "(") { // open paren after underscore
+                    equation = deleteSubstring(equation, underscoreIndex + 1, 1);
+                    equation = deleteSubstring(equation, closeParenIndex - 1, 1);
+                    closeParenIndex -= 2; // two parentheses deleted
+                }
+            //   console.log("equation: " + equation);
+                subscriptStartIndex = underscoreIndex + 1;
+                subscriptEndIndex = closeParenIndex + 1;
+            //   console.log("subscriptStartIndex: " + subscriptStartIndex);
+            //   console.log("subscriptEndIndex: " + subscriptEndIndex);
+            }
+            let subscript = "";
+            for (let i = subscriptStartIndex; i < subscriptEndIndex; i++) {
+
+                const char = equation.substring(i, i + 1);
+                // console.log("char: ", char);
+                if (char !== "(" && char !== ")" && char != "*") {
+                    subscript += char;
+                }
+            }
+            // console.log("subscript: " + subscript);
+            equation = insertSubstring(equation, subscriptStartIndex, subscript, subscriptEndIndex - underscoreIndex - 2);
+            startIndex = underscoreIndex + 1;
+            underscoreIndex = equation.indexOf("_", startIndex);
+        }
+        return equation;
+    } else {
+        return equation;
+    }
 }
 
 function AttachWarning(element) {
-    console.log('TCL: AttachWarning -> AttachWarning');
+    // console.log('TCL: AttachWarning -> AttachWarning');
     const warningSign = element.parentNode.querySelector(`.warning`);
     if (!warningSign) {
         element.insertAdjacentHTML("afterend", `<i class="fa fa-exclamation-triangle warning" aria-hidden="true"></i>`);
@@ -113,12 +174,12 @@ function AttachWarning(element) {
 }
 
 function detachWarning(element) {
-    console.log('TCL: detachWarning -> detachWarning');
-    console.log('TCL: detachWarning -> timerId', warningTimerId);
+    // console.log('TCL: detachWarning -> detachWarning');
+    // console.log('TCL: detachWarning -> timerId', warningTimerId);
     const warningSign = element.parentNode.querySelector(`.warning`);
     // clearTimeout(timerId);
     if (warningSign) {
-        console.log('TCL: detachWarning -> removing warningSign', warningSign);
+        // console.log('TCL: detachWarning -> removing warningSign', warningSign);
         warningSign.remove();
     } else {
         clearTimeout(warningTimerId);
@@ -127,7 +188,7 @@ function detachWarning(element) {
 
 function setEquation(equation) {
     if (equation) {
-        console.log('TCL: setEquation -> equation', equation);
+        console.log('TCL: setEquation -> convertSymbolToText(equation)', convertSymbolToText(equation));
         guppyInput.import_text(convertSymbolToText(equation));
         guppyInput.engine.end();
         guppyInput.activate();
@@ -151,7 +212,7 @@ function createEquationEditor() {
 }
 
 function removeExtraGuppyOSKTabs() {
-    console.log('TCL: removeExtraGuppyOSKTabs -> removeExtraGuppyOSKTabs');
+    // console.log('TCL: removeExtraGuppyOSKTabs -> removeExtraGuppyOSKTabs');
     // remove extra tabs
     document.querySelector('#guppy_osk_tab_calculus').remove();
     document.querySelector('#guppy_osk_tab_array').remove();
@@ -176,7 +237,7 @@ function removeExtraGuppyOSKTabs() {
         // remove extra greek letters
         console.log("removing extra greek letters...");
         document.querySelectorAll('#greek span > span.katex-mathml > math > semantics > mrow > mi').forEach(cell => {
-            console.log('TCL: removeExtraGuppyOSKTabs -> cell', cell);
+            // console.log('TCL: removeExtraGuppyOSKTabs -> cell', cell);
             const letter = cell.innerHTML;
             if (Object.values(conversionTable).indexOf(letter) < 0) {
                 cell.closest("span.guppy_osk_key").remove();
@@ -218,7 +279,7 @@ function openEquationEditField(eqName, eqInfo, position) {
     }
     guppyInput.event("focus", (focusedObj) => {
         if (focusedObj.focused) {
-            console.log('TCL: focused', focusedObj);
+            // console.log('TCL: focused', focusedObj);
             removeExtraGuppyOSKTabs();
             editor.scrollIntoView({
                 behavior: "smooth",
@@ -249,7 +310,7 @@ function openEquationEditField(eqName, eqInfo, position) {
         const vars = getEquationVars(eq);
         console.log('TCL: updateVarTable -> vars', vars);
         let varTable = document.getElementById("varTable");
-        console.log('TCL: updateVarTable -> varTable', varTable);
+        // console.log('TCL: updateVarTable -> varTable', varTable);
         if (varTable) {
             const rows = varTable.querySelectorAll(`tbody tr`);
             const rowVars = [];
@@ -258,7 +319,7 @@ function openEquationEditField(eqName, eqInfo, position) {
                     rowVars.push(row.getAttribute("data-var"));
                 }
             );
-            console.log('TCL: updateVarTable -> rowVars', rowVars);
+            // console.log('TCL: updateVarTable -> rowVars', rowVars);
             vars.forEach(
                 (variable) => {
                     if (rowVars.indexOf(variable) < 0) {
@@ -277,7 +338,7 @@ function openEquationEditField(eqName, eqInfo, position) {
                     }
                 );
             }
-            if (eq === "") {
+            if (!eq instanceof Error && eq === "") {
                 if (varTable) {
                     varTable.remove();
                 }
@@ -285,12 +346,17 @@ function openEquationEditField(eqName, eqInfo, position) {
         } else {
             createVarTable();
         }
+        renderEquationVars();
     }
 
     function getEquationVars(equation) {
         let variables = [];
         try {
-            variables = nerdamer(equation.replace(/\=/g, " ")).variables();
+            variables = nerdamer(handleSubscripts(equation.replace(/\=/g, " "), false)).variables();
+            console.log('TCL: getEquationVars -> variables', variables);
+            console.log("Handling subscripts in equation vars...");
+            variables = variables.map(variable => handleVarNameSubscripts(variable));
+			console.log('TCL: getEquationVars -> variables', variables);
             deleteStrInArray("ln", variables);
         } catch (e) {}
         return variables;
@@ -300,7 +366,7 @@ function openEquationEditField(eqName, eqInfo, position) {
         const eq = getEquation();
         detachWarning(eqInput);
         const vars = getEquationVars(eq);
-        console.log('TCL: createEquationEditor -> vars', vars);
+        // console.log('TCL: createEquationEditor -> vars', vars);
         let varTable = document.getElementById("varTable");
         if (vars.length > 0) {
             let tableStr = `
@@ -327,6 +393,7 @@ function openEquationEditField(eqName, eqInfo, position) {
                 // load var equations if specified
                 if (varEquations) {
                     loadVarEquations(varEquations);
+                    renderEquationVars();
                 }
                 // load var descriptions if specified
                 if (varDescriptions) {
@@ -337,8 +404,27 @@ function openEquationEditField(eqName, eqInfo, position) {
     }
 }
 
+function renderEquationVars() {
+    console.log('TCL: renderEquationVars -> renderEquationVars');
+    const result = Guppy.Doc.render_all("text", "$$");
+    console.log('TCL: renderEquationVars -> result', result);
+    const varTable = document.getElementById("varTable");
+    if (varTable) {
+        varTable.querySelectorAll("tbody tr").forEach(
+            (row) => {
+                console.log('TCL: renderEquationVars -> row', row);
+                const guppyDoc = row.querySelector(".guppy-render");
+                if (guppyDoc && guppyDoc.innerHTML === "ERROR: undefined") {
+                    row.querySelector("th").innerHTML = `\$\$${row.getAttribute("data-var")}\$\$`;
+                }
+            }
+        );
+        // Guppy.Doc.render_all("text", "$$");
+    }
+}
+
 function createNewRowHTML(variable) {
-    let newRow = `<tr data-var="${variable}"><th>${variable}</th><td>`;
+    let newRow = `<tr data-var="${variable}"><th>\$\$${variable}\$\$</th><td>`;
     // add variable equation column
     newRow += `<input type="text" size="${eqLength}" class="eqInput" data-var="${variable}" spellcheck="false"></td>`;
     // add variable description column
--- js/utils.js
+++ js/utils.js
@@ -9,6 +9,26 @@ function deleteStrInArray(str, array) {
     }
 }
 
+function findClosingBracketMatchIndex(str, pos) {
+    if (str[pos] != '(') {
+      throw new Error("No '(' at index " + pos);
+    }
+    let depth = 1;
+    for (let i = pos + 1; i < str.length; i++) {
+      switch (str[i]) {
+      case '(':
+        depth++;
+        break;
+      case ')':
+        if (--depth == 0) {
+          return i;
+        }
+        break;
+      }
+    }
+    return -1;    // No matching closing parenthesis
+  }
+
 /************************* From generateScript.js *************************/
 
 // source: https://stackoverflow.com/a/3561711/6798201
